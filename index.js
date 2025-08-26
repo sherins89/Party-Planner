450e9a71df837420aa1e5160d076b54de0c55b86
Sherin <sherin.daniel89@yahoo.com>
2025-08-25 22:42:06 -0400

    Test do not submit

---
+++ index.js
@@ -0,0 +1,11 @@
+const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+const COHORT = "2506-FTB-CT-WEB-PT";
+
+const API = `${BASE_URL}/${COHORT}`;
+console.log(API);
+
+// State //
+let 
+
+// Get App //
+const app = document.getElementById("app");
